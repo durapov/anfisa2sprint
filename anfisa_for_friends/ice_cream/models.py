1bfcd15d7b81eb6bf29aca814a569e273d1ffb59
Dmitry Durapov <ddurapov@gmail.com>
2023-07-29 14:10:29 +0300

    Admin preferences

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,27 +4,53 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(
+        default=100,
+        verbose_name='Порядок отображения'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self):
+        return self.title
+
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = 'Топпинг'
+        verbose_name_plural = 'Топпинги'
+    
+    def __str__(self):
+        return self.title 
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField(
+        'Название',
+        max_length=256,
+        help_text='Уникальное название обёртки, не более 256 символов'
+    )
+    class Meta:
+        verbose_name = 'Обертка'
+        verbose_name_plural = 'Обертки'
+
+    def __str__(self):
+        return self.title 
 
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    description = models.TextField('Описание')
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
-        related_name='ice_cream',
+        related_name='Wrapper',
+        verbose_name='Обертка',
         null=True,
         blank=True,
     )
@@ -32,7 +58,19 @@ class IceCream(PublishedModel):
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
-    )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+        verbose_name='Категория'
+        )
+    toppings = models.ManyToManyField(Topping, 
+                                      verbose_name='Топпинги'
+                                      )
+    is_on_main = models.BooleanField(
+        'На главную', 
+        default=False)
 
+    class Meta:
+        verbose_name = 'Мороженое'
+        verbose_name_plural = 'Мороженое'
+
+
+    def __str__(self):
+            return self.title 
